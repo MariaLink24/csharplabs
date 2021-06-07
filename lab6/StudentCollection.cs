59aec887300a8cf9df546c6d707bb0f28a8a235f
MariaLink24 <fromnarniawithlove@gmail.com>
2021-06-07 18:20:08 +0300

    lab 6

---
+++ lab6/StudentCollection.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentCollection  {
+
+private static List<Student> studentList;
+
+public double maxMean {
+    get {return studentList.Max(s=> s.meanValue);}
+}
+
+public IEnumerable<Student> specialists {
+    get {return studentList.Where(s=> s.educationType ==Education.Specialist);}
+}
+
+public StudentCollection(){
+    studentList = new List<Student>();
+}
+public void AddDefaults(){
+    studentList.Add(new Student());
+}
+public void AddStudents(Student[] students){
+    for (int i=0; i<students.Length; i++){
+        studentList.Add(students[i]);
+    }
+}
+
+public override string ToString()
+    {
+        string res = "";
+        if (studentList!=null){
+            foreach(Student s in studentList){
+                res += s.ToString() + " ";
+            }
+        }
+        return res;
+    }
+
+public virtual string ToShortString(){
+     string res = "";
+        if (studentList!=null){
+            foreach(Student s in studentList){
+                res += s.ToShortString() + " " + s.exams.Count + " " + s.tests.Count + " ";
+            }
+        }
+        return res;
+}
+public void compareByMeanValue(){
+    studentList.Sort((IComparer<Student>) new StudentComparator());
+
+}
+
+public void compareByBirthdate(){
+       studentList.Sort( (IComparer<Student>) new Person());
+
+}
+
+public void compareByLastName(){
+     studentList.Sort(delegate (Student x, Student y)
+            {   
+                return x.last.CompareTo(y.last);
+            });
+}
+
+public IOrderedEnumerable<IGrouping<double, Student>> AverageMarkGroup(double value){
+var queryLastNames =
+        from student in studentList
+        group student by student.meanValue==value into newGroup
+        select newGroup;
+return (IOrderedEnumerable<IGrouping<double, Student>>)queryLastNames;
+}
+
+
+}
+
