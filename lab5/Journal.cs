71f2e6faea2e870f20c4837c51a499811ff01dab
MariaLink24 <fromnarniawithlove@gmail.com>
2021-06-26 16:52:11 +0300

    add lab 5

--- lab5/Journal.cs
+++ lab5/Journal.cs
@@ -3,8 +3,19 @@ namespace testlab
 {
     public class Journal
     {
-        public Journal()
+   List<JournalEntry> changes = new List<JournalEntry>();
+
+        public void handler(object sender, StudentListHandlerEventArgs<string> e)
         {
+            changes.Add(new JournalEntry(e.NameOfCollection, e.Reason, e.PropertyName, e.RegisterNumber));
+        }
+
+        public override string ToString()
+        {
+            string text = "";
+            for (int i = 0; i < changes.Count; i++)
+                text += changes[i] + "\n";
+            return text;
         }
     }
 }
--- lab5/JournalEntry.cs
+++ lab5/JournalEntry.cs
@@ -3,8 +3,17 @@ namespace testlab
 {
     public class JournalEntry
     {
-        public JournalEntry()
+        string NameOfCollection { get; set; }
+        Update U { get; set; }
+        string PropertyName { get; set; }
+        int RegisterNumber { get; set; }
+        public JournalEntry(string nameOfCollection, Update update, string propertyName, int registredNumber)
         {
+            NameOfCollection = nameOfCollection; U = update; PropertyName = propertyName; RegisterNumber = registredNumber;
+        }
+        public override string ToString()
+        {
+            return "NameOfCollection: " + NameOfCollection + ", Update: " + Update + ", PropertyName: " + PropertyName + ", RegistredNumber: " + RegisterNumber;
         }
     }
 }
